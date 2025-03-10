807f2ddd37dcd1c9bf4ea78de23914c8588b9584
Lucas <lluc23@hotmail.com>
2025-03-10 17:34:44 +0100

    vendor in nfc to fix it

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,148 @@
 
 #include <LiquidCrystal_I2C.h>
 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+// If using the breakout with SPI, define the pins for SPI communication.
+#define PN532_SCK  (18)  // ESP32 SCK
+#define PN532_MISO (19)  // ESP32 MISO
+#define PN532_MOSI (23)  // ESP32 MOSI
+#define PN532_SS   (5)   // ESP32 CS
+// Use this line for a breakout with a software SPI connection (recommended):
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-void setup()
+void setupLCD(void)
 {
-  lcd.init();                      // initialize the lcd
-  // Print a message to the LCD.
+  lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("Hello, world!");
-   lcd.setCursor(0,1);
-  lcd.print("Arduino LCM IIC 2004");
+  lcd.print("Set up done");
+  lcd.setCursor(0,1);
+  lcd.print("Let's start");
+}
+
+void setupNFC(void) {
+  Serial.begin(115200);
+  while (!Serial) delay(10); // for Leonardo/Micro/Zero
+
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+    lcd.print(versiondata);
+  if (! versiondata) {
+    lcd.setCursor(0,1);
+    lcd.print("PN532 NOT FOUND");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  Serial.println("Waiting for an ISO14443A Card ...");
+  lcd.clear();
 }
 
 
-void loop()
+void setup()
 {
+  setupLCD();
+  setupNFC();
+  lcd.clear();
+  lcd.setCursor(0,1);
+    lcd.print("all set up");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
+  // the UID, and uidLength will indicate the size of the UUID (normally 7)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    lcd.clear();
+    lcd.print("inside the loop");
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+
+    if (uidLength == 7)
+    {
+      uint8_t data[32];
+
+      // We probably have an NTAG2xx card (though it could be Ultralight as well)
+      Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");
+
+      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
+      // starting at page 4 ... larger cards just add pages to the end of
+      // this range:
+
+      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
+
+      // TAG Type       PAGES   USER START    USER STOP
+      // --------       -----   ----------    ---------
+      // NTAG 203       42      4             39
+      // NTAG 213       45      4             39
+      // NTAG 215       135     4             129
+      // NTAG 216       231     4             225
+
+      for (uint8_t i = 0; i < 42; i++)
+      {
+        success = nfc.ntag2xx_ReadPage(i, data);
+
+        // Display the current page number
+        Serial.print("PAGE ");
+        if (i < 10)
+        {
+          Serial.print("0");
+          Serial.print(i);
+        }
+        else
+        {
+          Serial.print(i);
+        }
+        Serial.print(": ");
+
+        // Display the results, depending on 'success'
+        if (success)
+        {
+          // Dump the page data
+          nfc.PrintHexChar(data, 4);
+        }
+        else
+        {
+          Serial.println("Unable to read the requested page!");
+        }
+      }
+    }
+    else
+    {
+      Serial.println("This doesn't seem to be an NTAG203 tag (UUID length != 7 bytes)!");
+    }
+
+    // Wait a bit before trying again
+    Serial.println("\n\nSend a character to scan another tag!");
+    Serial.flush();
+    while (!Serial.available());
+    while (Serial.available()) {
+    Serial.read();
+    }
+    Serial.flush();
+  }
 }
