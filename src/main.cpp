374bbfea4e8fb069884b8f3727c5754de015941a
Lucas <lluc23@hotmail.com>
2025-03-30 13:07:55 +0200

    read more things

--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,124 @@
 #include <Arduino.h>
-
-#include <LiquidCrystal_I2C.h>
-
 #include <Wire.h>
 #include <SPI.h>
 #include <nfc_pn532.h>
+#include <LiquidCrystal_I2C.h>
 
-// If using the breakout with SPI, define the pins for SPI communication.
-// #define PN532_SCK  (18)  // ESP32 SCK
-// #define PN532_MISO (19)  // ESP32 MISO
-// #define PN532_MOSI (23)  // ESP32 MOSI
-// #define PN532_SS   (5)   // ESP32 CS
-// // Use this line for a breakout with a software SPI connection (recommended):
-// Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 #define SDA_PIN 21
 #define SCL_PIN 22
 
 Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
-// Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+LiquidCrystal_I2C lcd(0x27, 16, 2); // 16x2 LCD Display
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-void setupLCD(void)
-{
-  lcd.init();
-  lcd.backlight();
-}
-
-void setupNFC(void) {
-  Serial.begin(115200);
-  while (!Serial) delay(10); // for Leonardo/Micro/Zero
-
-  Serial.println("Hello!");
-
-  nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-
-  lcd.clear();
-  lcd.setCursor(0,0);
-    lcd.print(versiondata);
-  if (! versiondata) {
-    lcd.setCursor(0,1);
-    lcd.print("PN532 NOT FOUND");
-    while (1); // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-
-  Serial.println("Waiting for an ISO14443A Card ...");
-  lcd.clear();
-}
-
-
-void setup()
-{
-  setupLCD();
-  setupNFC();
-  lcd.clear();
-  lcd.setCursor(0,1);
-    lcd.print("all set up");
-}
-
-
-void loop(void) {
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-
-  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
-  // the UID, and uidLength will indicate the size of the UUID (normally 7)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-
-  if (success) {
+void setupLCD() {
+    lcd.init();
+    lcd.backlight();
     lcd.clear();
-    lcd.print("inside the loop");
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
+    lcd.setCursor(0, 0);
+    lcd.print("Initializing...");
+}
 
-    if (uidLength == 7)
-    {
-      uint8_t data[32];
+void setupNFC() {
+    Serial.begin(115200);
+    Serial.println("\n🔵 Starting NFC Module...");
+    Serial.printf("Chip Model: %s\n", ESP.getChipModel());
+    Serial.printf("Chip Revision: %d\n", ESP.getChipRevision());
+    Serial.printf("Number of Cores: %d\n", ESP.getChipCores());
 
-      // We probably have an NTAG2xx card (though it could be Ultralight as well)
-      Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");
 
-      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
-      // starting at page 4 ... larger cards just add pages to the end of
-      // this range:
+    nfc.begin();
+    uint32_t versiondata = nfc.getFirmwareVersion();
 
-      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
-
-      // TAG Type       PAGES   USER START    USER STOP
-      // --------       -----   ----------    ---------
-      // NTAG 203       42      4             39
-      // NTAG 213       45      4             39
-      // NTAG 215       135     4             129
-      // NTAG 216       231     4             225
-
-      for (uint8_t i = 0; i < 42; i++)
-      {
-        success = nfc.ntag2xx_ReadPage(i, data);
-
-        // Display the current page number
-        Serial.print("PAGE ");
-        if (i < 10)
-        {
-          Serial.print("0");
-          Serial.print(i);
-        }
-        else
-        {
-          Serial.print(i);
-        }
-        Serial.print(": ");
-
-        // Display the results, depending on 'success'
-        if (success)
-        {
-          // Dump the page data
-          nfc.PrintHexChar(data, 4);
-        }
-        else
-        {
-          Serial.println("Unable to read the requested page!");
-        }
-      }
-    }
-    else
-    {
-      Serial.println("This doesn't seem to be an NTAG203 tag (UUID length != 7 bytes)!");
+    if (!versiondata) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("❌ PN532 NOT FOUND");
+        Serial.println("❌ PN532 NOT FOUND");
+        while (1); // Halt execution
     }
 
-    // Wait a bit before trying again
-    Serial.println("\n\nSend a character to scan another tag!");
-    Serial.flush();
-    while (!Serial.available());
-    while (Serial.available()) {
-    Serial.read();
+    Serial.print("✅ Found PN532, Firmware: ");
+    Serial.print((versiondata >> 16) & 0xFF);
+    Serial.print(".");
+    Serial.println((versiondata >> 8) & 0xFF);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("PN532 Ready");
+}
+
+void readNtag2xx() {
+    uint8_t data[4];
+    Serial.println("🔹 Detected NTAG2xx tag (7-byte UID)");
+
+    for (uint8_t i = 0; i < 42; i++) {
+        if (nfc.ntag2xx_ReadPage(i, data)) {
+            Serial.print("PAGE ");
+            Serial.print(i);
+            Serial.print(": ");
+            nfc.PrintHexChar(data, 4);
+        } else {
+            Serial.print("PAGE ");
+            Serial.print(i);
+            Serial.println(": ❌ Unable to read");
+        }
     }
-    Serial.flush();
-  }
+}
+
+void readMifareClassic(uint8_t *uid, uint8_t uidLength) {
+    Serial.println("🔹 Detected MIFARE Classic Card");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("MIFARE Classic");
+
+    // Try to authenticate a block using default key (0xFFFFFFFFFFFF)
+    uint8_t keyA[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    if (nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keyA)) {
+        Serial.println("✅ Authentication successful!");
+    } else {
+        Serial.println("❌ Authentication failed!");
+    }
+}
+
+void loop() {
+    uint8_t success;
+    uint8_t uid[7];  // Buffer for UID
+    uint8_t uidLength;
+
+    Serial.println("\n🔄 Scanning for an NFC tag...");
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+    if (success) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Card Detected");
+
+        Serial.println("✅ Found an ISO14443A card");
+        Serial.print("  UID Length: ");
+        Serial.print(uidLength);
+        Serial.println(" bytes");
+        Serial.print("  UID Value: ");
+        nfc.PrintHex(uid, uidLength);
+        Serial.println("");
+
+        if (uidLength == 4) {
+            Serial.println("🔹 Possible MIFARE Classic or Ultralight");
+            readMifareClassic(uid, uidLength);
+        }
+        else if (uidLength == 7) {
+            Serial.println("🔹 Likely an NTAG or MIFARE Ultralight");
+            readNtag2xx();
+        }
+        else {
+            Serial.println("🔹 Unknown card type");
+        }
+
+        Serial.println("\n🔄 Place another card to scan again...");
+        delay(2000);
+    }
+}
+
+void setup() {
+    setupLCD();
+    setupNFC();
 }
